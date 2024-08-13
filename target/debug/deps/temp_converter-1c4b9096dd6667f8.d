36787b44182b228fc738bb71fe4c91b8f6973060
ibrainiac <franqhazard@gmail.com>
2024-08-14 01:49:53 +0300

    simple logic written in Rust to convert temperature to celcius or farenheight

---
+++ target/debug/deps/temp_converter-1c4b9096dd6667f8.d
@@ -0,0 +1,5 @@
+/Users/mac/rust/temp_converter/target/debug/deps/libtemp_converter-1c4b9096dd6667f8.rmeta: src/main.rs
+
+/Users/mac/rust/temp_converter/target/debug/deps/temp_converter-1c4b9096dd6667f8.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/temp_converter-48439c6596d6a5d8.d
@@ -0,0 +1,5 @@
+/Users/mac/rust/temp_converter/target/debug/deps/libtemp_converter-48439c6596d6a5d8.rmeta: src/main.rs
+
+/Users/mac/rust/temp_converter/target/debug/deps/temp_converter-48439c6596d6a5d8.d: src/main.rs
+
+src/main.rs:
